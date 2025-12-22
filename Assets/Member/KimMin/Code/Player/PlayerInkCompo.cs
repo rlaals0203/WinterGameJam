cd002515db6097106fd648268c1144a80c24977c
rlaals0203 <scientistmin@gmail.com>
2025-12-22 17:42:50 +0900

    fix : ui asset

---
+++ Assets/Member/KimMin/Code/Player/PlayerInkCompo.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+
+namespace Code.Entities
+{
+    public class PlayerInkCompo : MonoBehaviour, IEntityComponent
+    {
+        
+        public void Initialize(Entity entity)
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
