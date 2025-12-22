d97d94ae7336e84026e5169ec9b26a39bb56f327
rlaals0203 <scientistmin@gmail.com>
2025-12-22 11:00:55 +0900

    feat : title

---
+++ Assets/Member/KimMin/UI/Misc/VolumeSetting.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+namespace KimMin.UI.Misc
+{
+    public class VolumeSetting
+    {
+        [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] private Slider masterSlider;
+        [SerializeField] private Slider musicSlider;
+        [SerializeField] private Slider sfxSlider;
+
+        private void Start()
+        {
+            if (PlayerPrefs.HasKey("musicVolume") && PlayerPrefs.HasKey("sfxVolume") && PlayerPrefs.HasKey("masterVolume"))
+                LoadVolume();
+            else
+            {
+                SetMasterVolume();
+                SetMusicVolume();
+                SetSfxVolume();
+            }
+        }
+
+        public void SetMasterVolume()
+        {
+            float volume = masterSlider.value;
+            audioMixer.SetFloat("masterVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("masterVolume", volume);
+        }
+
+        public void SetMusicVolume()
+        {
+            float volume = musicSlider.value;
+            audioMixer.SetFloat("musicVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("musicVolume", volume);
+        }
+
+        public void SetSfxVolume()
+        {
+            float volume = sfxSlider.value;
+            audioMixer.SetFloat("soundFXVolume", Mathf.Log10(volume) * 20);
+            PlayerPrefs.SetFloat("sfxVolume", volume);
+        }
+
+        private void LoadVolume()
+        {
+            masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+
+            SetMasterVolume();
+            SetMusicVolume();
+            SetSfxVolume();
+        }
+    }
+}
