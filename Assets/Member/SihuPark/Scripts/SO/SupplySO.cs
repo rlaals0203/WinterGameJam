97495a6b1941a050f0e5af42b8831056a81b2b91
SihuPark <sihuop0823@gmail.com>
2025-12-22 23:50:20 +0900

    Add | 상점 Supply SO 추가

--- Assets/Member/SihuPark/Scripts/SO/SupplySO.cs
+++ Assets/Member/SihuPark/Scripts/SO/SupplySO.cs
@@ -1,16 +1,24 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Supply", menuName = "SO/Supply", order = 0)]
+public enum SupplyType
+{
+    Palette,
+    Brush,
+    Extractor
+}
+
+[CreateAssetMenu(fileName = "New Supply", menuName = "Shop/Supply Item", order = 1)]
 public class SupplySO : ScriptableObject
 {
     [Header("Basic Info")]
     public string SupplyName;
     public int SupplyID;
+    public SupplyType supplyType;
 
     [Header("Display")]
-    public Sprite icon; // 아이템 이미지
+    public Sprite icon;
     [TextArea(3, 5)]
-    public string description; // 아이템 설명
+    public string description;
 
     [Header("Economy")]
     public int price;
