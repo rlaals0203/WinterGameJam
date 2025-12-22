23a846ac63385dcbf08023bbff8be64a1f66256e
PSJ7 <parksungje0213@gmail.com>
2025-12-22 21:30:40 +0900

    fix : SJ

---
+++ Assets/Member/Sungje/Sungje/01.Script/Enemies/EnemyDataSO.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemies data", menuName = "SO/EnemyData")]
+public class EnemyDataSO : ScriptableObject
+{
+    public string enemyName;
+    [TextArea]
+    public string description;
+
+    public int maxHealth;
+    public int damage;
+
+    public float attackCooldown;
+    public float attackRange;
+    public int occupySpaceX;
+    public int occupySpaceY;
+}
