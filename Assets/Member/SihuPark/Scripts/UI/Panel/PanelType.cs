cc28fb5848bb3ac344295c9f71840fae6a1c6858
SihuPark <sihuop0823@gmail.com>
2025-12-22 15:37:41 +0900

    Add | Panel 구조 추가

---
+++ Assets/Member/SihuPark/Scripts/UI/Panel/PanelType.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// <para>Default : 기본적인 UI</para>
+/// <para>Popup : 작게 뜨거나 작게 뜨는 UI 예) 정말로 종료하시겠습니까?</para>
+/// </summary>
+/// 
+public enum PanelType
+{
+    Default,
+    Popup,
+}
