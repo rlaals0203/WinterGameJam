02dc5b94f9322601c689e95a22ecf683101ccc93
PSJ7 <parksungje0213@gmail.com>
2025-12-23 00:36:50 +0900

    fix

--- Assets/Member/Sungje/01.Script/Enemies/EnemyAttackCompo.cs
+++ Assets/Member/Sungje/01.Script/Enemies/EnemyAttackCompo.cs
@@ -1,48 +1,57 @@
+using Code.Entities;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EnemyAttackCompo : MonoBehaviour
 {
-    [SerializeField] private Transform batPivot;
-    [SerializeField] private float swingSpeed = 720f;
-    [SerializeField] private float swingAngle = 120f;
+    [SerializeField] private LayerMask targetLayer;
 
-    private float _currentAngle;
-    private bool _isSwinging;
     private Enemy _enemy;
+    private bool _isAttacking;
+
+    private EnemyDataSO Data => _enemy.enemyDataSO;
 
     private void Awake()
     {
         _enemy = GetComponent<Enemy>();
-        batPivot.gameObject.SetActive(false);
-    }
-
-    private void Update()
-    {
-        if (!_isSwinging) return;
-
-        float delta = swingSpeed * Time.deltaTime;
-        _currentAngle += delta;
-
-        if (_currentAngle >= swingAngle)
-        {
-            EndAttack();
-            return;
-        }
-
-        batPivot.localRotation = Quaternion.Euler(0, 0, -_currentAngle);
     }
 
     public void StartAttack()
     {
-        _currentAngle = 0f;
-        _isSwinging = true;
-        batPivot.localRotation = Quaternion.identity;
-        batPivot.gameObject.SetActive(true);
+        _isAttacking = true;
+    }
+
+    public void DoAttack()
+    {
+        if (!_isAttacking) return;
+        if (Data == null) return;
+
+        Collider2D hit = Physics2D.OverlapCircle(
+            transform.position,
+            Data.attackRange,
+            targetLayer
+        );
+
+        if (hit == null) return;
+
+        Player target = hit.GetComponent<Player>();
+        if (target == null) return;
+
+        target.TakeDamage(Data.damage);
     }
 
     public void EndAttack()
     {
-        _isSwinging = false;
-        batPivot.gameObject.SetActive(false);
+        _isAttacking = false;
     }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        if (_enemy == null || _enemy.enemyDataSO == null) return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, _enemy.enemyDataSO.attackRange);
+    }
+#endif
 }
