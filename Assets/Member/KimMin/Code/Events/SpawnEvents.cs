63b89d8bf3c09e6108038f180826913cde6d6c61
PSJ7 <parksungje0213@gmail.com>
2025-12-23 11:57:55 +0900

    feat : EnergyBallEnemy

--- Assets/Member/KimMin/Code/Events/SpawnEvents.cs
+++ Assets/Member/KimMin/Code/Events/SpawnEvents.cs
@@ -7,6 +7,7 @@ namespace KimMin.Events
     public class SpawnEvents
     {
         public static SpawnBulletEvent SpawnBulletEvent = new();
+        public static SpawnEnergyBallEvent SpawnEnergyBallEvent = new();
     }
     
     public class SpawnBulletEvent : GameEvent
@@ -19,6 +20,27 @@ namespace KimMin.Events
         public Entity entity;
 
         public SpawnBulletEvent Init(PoolItemSO poolItem, Vector3 position, 
+            Vector3 rotation, float speed, float damage, Entity owner)
+        {
+            this.poolItem = poolItem;
+            this.position = position;
+            this.direction = rotation;
+            this.speed = speed;
+            this.damage = damage;
+            this.entity = owner;
+            return this;
+        }
+    }
+
+    public class SpawnEnergyBallEvent : GameEvent
+    {
+        public PoolItemSO poolItem;
+        public Vector3 position;
+        public Vector3 direction;
+        public float speed;
+        public float damage;
+        public Entity entity;
+        public SpawnEnergyBallEvent Init(PoolItemSO poolItem, Vector3 position, 
             Vector3 rotation, float speed, float damage, Entity owner)
         {
             this.poolItem = poolItem;
