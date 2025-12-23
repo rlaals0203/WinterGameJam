6bd5bfedea608945e0bde49f5e5b4232ae06d100
SihuPark <sihuop0823@gmail.com>
2025-12-23 11:19:05 +0900

    Add | 버튼 UI 수정 및 인스펙터 창에 SO 정렬

---
+++ Assets/Member/SihuPark/Scripts/UI/ChangeReadyPage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ChangeReadyPage : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
