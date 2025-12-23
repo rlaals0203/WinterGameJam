9936d6d51f9eeb33d35521a611c5549b1500977c
PSJ7 <parksungje0213@gmail.com>
2025-12-24 06:50:40 +0900

    fix : Missing Component

--- Assets/Member/Sungje/01.Script/Enemies/EnemyAttackCompo.cs
+++ Assets/Member/Sungje/01.Script/Enemies/EnemyAttackCompo.cs
@@ -5,41 +5,68 @@ public class EnemyAttackCompo : MonoBehaviour, IEntityComponent
 {
     [SerializeField] private LayerMask targetLayer;
     [SerializeField] private Animator animator;
+    [SerializeField] private float attackDuration = 0.4f;
 
     protected Enemy _enemy;
     private float _lastAttackTime;
+    private float _attackEndTime;
     private bool _isAttack;
-
-    public bool IsAttack => _isAttack;
+    private bool _isMove;
 
     private readonly int IsAttackHash = Animator.StringToHash("IsAttack");
+    private readonly int IsMoveHash = Animator.StringToHash("IsMove");
 
-    protected EnemyDataSO Data => _enemy.EnemyDataSO;
+    protected EnemyDataSO Data => _enemy?.EnemyDataSO;
+
+    private Animator Animator
+    {
+        get
+        {
+            if (animator == null)
+                animator = GetComponentInChildren<Animator>(true);
+            return animator;
+        }
+    }
 
     public void Initialize(Entity entity)
     {
         _enemy = entity as Enemy;
-        if (animator == null)
-            animator = GetComponentInChildren<Animator>();
+        _isMove = true;
     }
 
     private void Update()
     {
         TryDoAttack();
+        UpdateAttackTimer();
         UpdateMotion();
     }
 
     public void TryDoAttack()
     {
         if (_isAttack) return;
-        if (Data == null || _enemy.IsSpoilMode) return;
+        if (_enemy == null || Data == null) return;
+        if (_enemy.IsSpoilMode) return;
         if (Time.time - _lastAttackTime < Data.attackCooldown) return;
         if (_enemy.DistanceToPlayer > Data.attackRange) return;
 
         _lastAttackTime = Time.time;
+        _attackEndTime = Time.time + attackDuration;
+
         _isAttack = true;
+        _isMove = false;
 
         ProcessAttack();
+    }
+
+    private void UpdateAttackTimer()
+    {
+        if (!_isAttack) return;
+
+        if (Time.time >= _attackEndTime)
+        {
+            _isAttack = false;
+            _isMove = true;
+        }
     }
 
     protected virtual void ProcessAttack()
@@ -58,26 +85,11 @@ public class EnemyAttackCompo : MonoBehaviour, IEntityComponent
         target.TakeDamage(Data.damage);
     }
 
-    public void EndAttack()
-    {
-        _isAttack = false;
-    }
-
     private void UpdateMotion()
     {
-        animator.SetBool(IsAttackHash, _isAttack);
-    }
+        if (Animator == null) return;
 
-#if UNITY_EDITOR
-    private void OnDrawGizmosSelected()
-    {
-        if (_enemy == null || _enemy.EnemyDataSO == null) return;
-
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(
-            transform.position,
-            _enemy.EnemyDataSO.attackRange
-        );
+        Animator.SetBool(IsAttackHash, _isAttack);
+        Animator.SetBool(IsMoveHash, _isMove);
     }
-#endif
 }
