8c0ff6ec1ee5b669b31b68d6582eab94d0290666
SihuPark <sihuop0823@gmail.com>
2025-12-24 00:56:08 +0900

    Test 한거 지우기

--- Assets/Member/KimMin/Code/Core/InkStorage.cs
+++ Assets/Member/KimMin/Code/Core/InkStorage.cs
@@ -7,16 +7,16 @@ namespace Code.Core
 {
     public class InkStorage : MonoSingleton<InkStorage>
     {
-        private void Awake()
-        {
-            // 임의 테스트
-            _inkDict.Add(InkType.Red, 100);
-            _inkDict.Add(InkType.Blue, 200);
-            _inkDict.Add(InkType.Yellow, 300);
-            _inkDict.Add(InkType.Black, 400);
-            _inkDict.Add(InkType.White, 500);
-            _inkDict.Add(InkType.Green, 600);
-        }
+        //private void Awake()
+        //{
+        //    // 임의 테스트
+        //    _inkDict.Add(InkType.Red, 100);
+        //    _inkDict.Add(InkType.Blue, 200);
+        //    _inkDict.Add(InkType.Yellow, 300);
+        //    _inkDict.Add(InkType.Black, 400);
+        //    _inkDict.Add(InkType.White, 500);
+        //    _inkDict.Add(InkType.Green, 600);
+        //}
 
         private Dictionary<InkType, int> _inkDict = new();
 
